16752b58ed931c2612a1d5b015b5d8f59691210c
Prajakta Pawar <prajkta2554@gmail.com>
2025-08-25 10:24:34 +0530

    update something

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import VideoSlider from './components/VideoSlider';
